7cc5c294d0b6ce774b480ddbd3ab6ad9ddd1301a
Bearslay <barfunkel@protonmail.com>
2024-12-31 19:57:04 -0500

    added two 2d vector classes; one only stores components and is better for storage and the other stores components, magnitude, and angle for quicker information recalling

--- bengine/bengine.hpp
+++ bengine/bengine.hpp
@@ -6,6 +6,8 @@
 #include "bengine_mouse.hpp"
 #include "bengine_loop.hpp"
 #include "bengine_helpers.hpp"
+#include "bengine_small_vector_2d.hpp"
+#include "bengine_fast_vector_2d.hpp"
 #include "bengine_colliders.hpp"
 #include "bengine_physics.hpp"
 
